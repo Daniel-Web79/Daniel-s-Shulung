31a875ddd58a8f3152685392bb2d16938e956e76
Daniel-Web79 <80534206+Daniel-Web79@users.noreply.github.com>
2021-09-03 17:15:54 +0200

    Initial project Commit

---
+++ VollerFehler/src/VollerFehler.java
@@ -0,0 +1,9 @@
+public class VollerFehler {
+	
+	public static void main(String[] args) {
+		System.out.println("Dieses Programm ist gleich fehlerfrei.");
+		System.out.println("Gitten testen.");
+		System.out.println("misst");
+		
+	}
+}
